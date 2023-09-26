dc61f82be1a16069737d06415539620798b45d16
Shane Pelletier <shane.pelletier@canonical.com>
2023-09-26 17:43:52 -0300

    Make terminal blocks span multiple pages if needed

--- custom_conf.py
+++ custom_conf.py
@@ -156,11 +156,11 @@ latex_elements = {
 \usepackage{charter}
 \usepackage[defaultsans]{lato}
 \usepackage{inconsolata}
-\usepackage{tcolorbox}
+\usepackage[most]{tcolorbox}
 \definecolor{yellowgreen}{RGB}{154, 205, 50}
 \newenvironment{sphinxclassprompt}{\color{yellowgreen}}{}
 \tcbset{colback=black, fontupper=\color{white}}
-\newenvironment{sphinxclassterminal}{\color{white}\sphinxsetup{VerbatimColor={black}}\begin{tcolorbox}}{\end{tcolorbox}}
+\newenvironment{sphinxclassterminal}{\color{white}\sphinxsetup{VerbatimColor={black}}\begin{tcolorbox}[enhanced, breakable, fontupper=\color{white}, use color stack=true]}{\end{tcolorbox}}
 ''',
     "sphinxsetup": 'verbatimwithframe=false, pre_border-radius=0pt, verbatimvisiblespace=\\phantom{}, verbatimcontinued=\\phantom{}',
 }
